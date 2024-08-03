69919037abdeb677a051e41da5e8790339e1a71d
Sebas5158R <sebastianriveraa4@gmail.com>
2024-08-03 18:21:10 -0500

    chore: Added tailwind integration, added typography and update dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        TradeGothic: ['Trade Gothic', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
